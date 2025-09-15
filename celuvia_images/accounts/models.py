44c63dd3d7b37b4b3412bb7b4d97db3573d58559
Anthony Nelson <anthonyjnelson@hotmail.com>
2025-09-15 22:19:56 +0100

    Add ResetToken model to accounts/models.py

--- celuvia_images/accounts/models.py
+++ celuvia_images/accounts/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
 
 
 class User(AbstractUser):
@@ -10,7 +11,7 @@ class User(AbstractUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     full_name = models.CharField(max_length=100, blank=True)
-    email= models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=20, blank=True)
     address_1 = models.CharField(max_length=50, blank=True)
     address_2 = models.CharField(max_length=50, blank=True)
@@ -25,9 +26,17 @@ class User(AbstractUser):
         if not self.full_name:
             self.full_name = f"{self.first_name} {self.last_name}"
         super().save(*args, **kwargs)
-    
+
     def is_vendor(self):
         return self.groups.filter(name="Vendors").exists()
 
     def is_buyer(self, *args, **kwargs):
         return self.groups.filter(namw="Buyers").exists()
+
+
+class ResetToken(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    token = models.CharField(max_length=500)
+    expiry_date = models.DateTimeField()
+    used = models.BooleanField(default=False)
