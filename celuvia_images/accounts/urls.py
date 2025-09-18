16d14d9bc164dacdec812295aada1293ab80920a
Anthony Nelson <anthonyjnelson@hotmail.com>
2025-09-18 17:18:10 +0100

    Add password reset URLs to accounts/urls.py

--- celuvia_images/accounts/urls.py
+++ celuvia_images/accounts/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path("signup/", views.buyer_signup, name="signup_buyer"),
     path("signup/vendor/", views.vendor_signup, name="signup_vendor"),
     path("login/", views.custom_login, name="login"),
+    path("reset/", views.request_password_reset,
+         name="password_reset_request"),
+    path("reset/<str:token>/", views.reset_password,
+         name="reset_password"),
 ]
